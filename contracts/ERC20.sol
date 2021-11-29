e5eaab90203d1140a2bf0a13a18c303a2d1455af
Hadi Dadjuy <hadidadjuy@yahoo.com>
2021-11-29 09:16:13 +0330

    adds ERC20 logic

---
+++ contracts/ERC20.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import './IERC20.sol';
+
+contract ERC20 is IERC20{
+    
+    string _name;
+    string _symbol;
+    uint _totalSupply;
+    mapping (address => uint) balances;
+    mapping (address => mapping(address => uint)) allowances;
+
+
+    function name() external view returns(string memory) {
+        return _name;        
+    }
+
+    function symbol() external view returns(string memory) {
+        return _symbol;        
+    }
+
+    function totalSupply() external view returns(uint) {
+        return _totalSupply;        
+    }
+
+    function balanceOf(address who) external view returns (uint256){
+        return balances[who];
+    }
+
+    function allowance(address owner, address spender) external view returns (uint){
+        return allowances[owner][spender];
+    }
+
+    function transfer(address to, uint value) external returns (bool){
+        require(value > 0 ,"ERROR: VALUE IS ZERO!");
+        require(balances[msg.sender] >= value ,"ERROR: INSUFFICIENT BALANCE!");
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender,to,value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) external returns (bool){
+        require(value > 0 ,"ERROR: VALUE IS ZERO!");
+        allowances[msg.sender][spender]=value;
+        emit Approval(msg.sender,spender,value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) external returns (bool){
+        require(value > 0 ,"ERROR: VALUE IS ZERO!");
+        require(allowances[from][msg.sender] >= value ,"ERROR: VALUE IS ZERO!");
+        allowances[from][msg.sender] -= value;
+        balances[from] -= value;
+        balances[to] += value;
+        emit Transfer(from,to,value);
+        return true;
+    }
+
+}
