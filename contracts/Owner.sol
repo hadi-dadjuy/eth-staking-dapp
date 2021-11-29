689a75f543589b638180cba6666e7b476ed069f9
Hadi Dadjuy <hadidadjuy@yahoo.com>
2021-11-29 09:32:43 +0330

    adds Owner contract

---
+++ contracts/Owner.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+
+
+contract Owner {
+    address owner;
+    
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function changeOwner(address newOwner) external onlyOwner returns(bool) {
+        owner = newOwner;
+    }
+   
+   modifier onlyOwner(){
+       require(owner == msg.sender , "ERROR: CALLER IS NOT OWNER");
+       _;
+   }
+}
